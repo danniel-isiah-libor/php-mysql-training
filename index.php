84eb7da8e93fa40f65903e9859a5b18b75fe1769
jaypeesanchez <jaypeesanchez@gmail.com>
2025-08-29 12:51:04 +0800

    Initial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+//echo "Hello World";
+// echo "<h1 style='color: green'> HELLO WORLD </h1>";
+echo 20;
+
+?>
+
+</body>
+</html>
+
+
