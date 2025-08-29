26116a2a9768f83f3eb63299010f6ad0bc913c96
aaoaaaaia <aaoaaaaia@gmail.com>
2025-08-29 12:48:29 +0800

    feat: initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // echo "<h1 style='color: blue'>ZA, WARUDO!</h1>";
+        // echo 3;
+        // echo True;
+        // var_dump(["hmm","skfjs"]);
+        echo "Yellow";
+    ?>
+</body>
+</html>
