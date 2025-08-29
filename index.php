e28b6d1c4fd420bc9727ec68763ebf5b40215b15
killuasoft <ikilluasoft@yahoo.com>
2025-08-29 12:49:52 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP</title>
+</head>
+<body>
+    <?php
+        //echo "My First PHP website!";
+        // echo "<h1 style='color:red'>Hello PHP World!</h1>";
+        // echo 20;
+        // echo true;
+        // echo "a" . "b";
+        $name="Resty Yap";
+        for ($i=0;$i<5;$i++)
+        {
+            echo $name . "<br>";
+        }
+        echo "<br>";
+        var_dump(["Honda","Toyota"]);
+
+        $color="red";
+        echo $color . "<br>";
+    ?>    
+</body>
+</html>
